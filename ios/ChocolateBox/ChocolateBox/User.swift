d41a3dce560f35850b3909e8125341438c0e8d8d
Timothy <ttkim@wesleyan.edu>
2016-06-05 00:34:57 -0400

    iOS - adding frameworks and Podfile

---
+++ ios/ChocolateBox/ChocolateBox/User.swift
@@ -0,0 +1,33 @@
+//
+//  User.swift
+//  ChocolateBox
+//
+//  Created by Tim Kim on 6/5/16.
+//  Copyright © 2016 259Williams. All rights reserved.
+//
+
+
+
+/*
+ParseUser user = new ParseUser();
+user.setUsername(etUserName.getText().toString());
+user.setPassword(etPassword.getText().toString());
+
+
+login_progress.setVisibility(View.VISIBLE);
+user.signUpInBackground(new SignUpCallback() {
+    @Override
+    public void done(ParseException e) {
+        login_progress.setVisibility(View.GONE);
+        
+        if (e == null) {
+            Toast.makeText(LoginActivity.this, "Registration OK",
+                Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(LoginActivity.this,
+                "Registration failed: " + e.getMessage(),
+                Toast.LENGTH_SHORT).show();
+        }
+    }
+    });
+*/
