5ff420b810956d8266a802b8f55358d59809d6cf
Timothy <ttkim@wesleyan.edu>
2016-06-04 23:56:09 -0400

    iOS - initialization

---
+++ ios/ChocolateBox/ChocolateBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChocolateBox
+//
+//  Created by Tim Kim on 6/4/16.
+//  Copyright © 2016 259Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
